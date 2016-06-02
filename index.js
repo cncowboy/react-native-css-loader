a1f16983068289fcad2848d2e10c761d813f4fe1
zhangxiaobing <zhangxiaobing@51yund.com>
2016-06-02 14:22:11 +0800

    can use react-native-extended-stylesheet

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ module.exports = function(source) {
 
 	var style = parseCss(source.replace(/\r?\n|\r/g, ""));
 
-	var prefix = "", suffix = "";
-	prefix = "var React = require('react-native');\nmodule.exports = React.StyleSheet.create(";
+    var moduleName = "react-native"
+    var objectName = "StyleSheet"
+	if (this.withExtendedStyleSheet) {
+        moduleName = "react-native-extended-stylesheet"
+        objectName = "EStyleSheet"
+    }
+
+	prefix = "var " + objectName + " = require('" + moduleName + "')." + objectName + ";\nmodule.exports = " + objectName + ".create(";
 	suffix = ");";
+	//var prefix = "", suffix = "";
+	//prefix = "var React = require('react-native');\nmodule.exports = React.StyleSheet.create(";
+	//suffix = ");";
 	var content = prefix + style + suffix
     return content;
 };
